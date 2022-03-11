9875c507a9829418641835db4079978340f0a5d4
NorthAlone <norbertlawadankossi@gmail.com>
2022-03-10 16:09:23 -0800

    add script 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include < stdio.h >
+/**
+ *main - print sentence
+ *
+ * return: always 0 when all is done correctly
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
