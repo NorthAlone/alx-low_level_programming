3ffeff859851c2626ce4161ddb1ab631834ff2ca
NorthAlone <norbertlawadankossi@gmail.com>
2022-03-11 15:13:47 -0800

    add script 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers of base 10 starting from 0
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
