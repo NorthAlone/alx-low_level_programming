09f8f2804669909ba18e83ba436c6e9e03f2c3ac
NorthAlone <norbertlawadankossi@gmail.com>
2022-03-11 13:33:34 -0800

    add script 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase
+ * Return: 0 for success
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; a <= 'z'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+
+	return (0);
+}
