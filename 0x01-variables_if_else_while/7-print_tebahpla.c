7f829ee8707ebf918fc99f9aa0fb78a6fdc3161d
NorthAlone <norbertlawadankossi@gmail.com>
2022-03-11 15:44:03 -0800

    add script 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase
+ * Return: 0 for success
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
