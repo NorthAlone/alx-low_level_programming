11df40d81cc769487cecf7e84a923930bb61b6fe
NorthAlone <norbertlawadankossi@gmail.com>
2022-03-10 17:08:17 -0800

    3 update script 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Print sentence
- *
- * return: Always 0 when all is done correctly
+ * Return: always 0 when done correctly
  */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
