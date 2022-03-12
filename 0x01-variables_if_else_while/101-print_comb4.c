9b8ea6238a03fdf06f9ac7d31625e7dfc013d9af
NorthAlone <norbertlawadankossi@gmail.com>
2022-03-11 18:16:31 -0800

    add script 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - print all possible combinations of single-digit numbers
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			for (z = 0; z <= 9; z++)
+			{
+				if (x != y != z)
+				{
+					if (y > x && z > y)
+					}
+						putchar(x + '0');
+						putchar(y + '0');
+						putchar(z + '0');
+						if ((x * y * z) < 503)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+			}	}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
