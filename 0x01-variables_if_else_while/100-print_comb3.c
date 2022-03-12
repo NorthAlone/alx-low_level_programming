5e7266763810bf90c6fd772e2332aac1a1a36f85
NorthAlone <norbertlawadankossi@gmail.com>
2022-03-11 17:25:29 -0800

    addd script 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - print all possible combinations of single-digit numbers
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			if (x != y)
+			{
+				if (y > x)
+				{
+					putchar(x + '0');
+					putchar(y + '0');
+					if ((int)(x + '' + y) != 89)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
