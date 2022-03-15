065d7d28111ab3411ddfe6d5327d90cf233c8c58
NorthAlone <norbertlawadankossi@gmail.com>
2022-03-15 14:49:34 -0700

    update 4 script 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,22 +11,3 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
-
-/**
- * print_alphabet - print alphabet in lower case
- *
- * Return: void
- *
- **/
-
-void print_alphabet(void)
-{
-	char ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		_putchar(ch);
-	}
-
-	_putchar('\n');
-}
