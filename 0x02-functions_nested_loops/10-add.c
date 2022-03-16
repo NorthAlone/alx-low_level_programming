3a10ddc6ef7ecc3d0feb51c22b022dd33586edd3
NorthAlone <norbertlawadankossi@gmail.com>
2022-03-16 07:35:08 -0700

    add script 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - entry point
+ * Description: adds two integers and returns the result.
+ * Return: sum off a and b
+ * @a: First integer
+ * @b: Second integer
+ **/
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
