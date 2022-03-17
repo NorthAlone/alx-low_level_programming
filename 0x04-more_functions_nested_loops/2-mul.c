b8d9df458d7af6779a3490ec8b9a6fb1d7e392f4
NorthAlone <norbertlawadankossi@gmail.com>
2022-03-17 16:03:56 -0700

    add script 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ * Description: multiplies two integers
+ * Return: void
+ * @a: first integer
+ * @b: second integer
+ **/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
