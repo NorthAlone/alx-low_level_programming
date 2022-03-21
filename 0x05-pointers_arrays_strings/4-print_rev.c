1b8bb027c43807d3e268e93db02d077346993325
NorthAlone <norbertlawadankossi@gmail.com>
2022-03-21 15:32:34 -0700

    add script 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_rev - entry point
+* Description : prints a string, in reverse, followed by a new line.
+* @s: String to print in reverse order
+* Return:void
+*/
+
+void print_rev(char *s)
+{
+	int len, i;
+
+	len = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		_putchar(s[len--]);
+	}
+	_putchar('\n');
+}
