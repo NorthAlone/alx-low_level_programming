69798bf7c662fc50c3a14394a8ad86f935efd81f
NorthAlone <norbertlawadankossi@gmail.com>
2022-03-21 14:42:14 -0700

    add script 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* int _strlen - entry point
+* Description : returns the length of a string.
+* @s: String to return length
+* Return: int value
+*/
+
+int _strlen(char *s)
+{
+	int len, i;
+
+	for (i < 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	return (len);
+}
