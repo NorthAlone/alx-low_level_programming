fac4690ba265de9de7e14f1d64e957b719eed7a7
NorthAlone <norbertlawadankossi@gmail.com>
2022-03-21 14:15:46 -0700

    add script 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* void swap_int - entry point
+* Description: swaps the values of two integers.
+* Return: void
+* @a: first value
+* @b: second value
+*/
+
+void swap_int(int *a, int *b);
+{
+	int temp;
+	temp = a;
+	*a = b;
+	*b = temp;
+}
