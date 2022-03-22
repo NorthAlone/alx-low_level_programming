e482e9b16d0d6b497b18a4042ab8da2b87b28169
NorthAlone <norbertlawadankossi@gmail.com>
2022-03-21 18:15:01 -0700

    add script 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* rev_string - reverse a string
+* @s: string to reverse
+* Return:void
+*/
+
+
+void rev_string(char *s)
+{
+	int i, j;
+	char ch[];
+
+	i = 0;
+	j = 0;
+
+	while (*(s + i))
+	{
+		*(a + i) = *(s + i);
+		i++;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		*(s + i) = *(a + j);
+		j++;
+		i--;
+	}
+}
