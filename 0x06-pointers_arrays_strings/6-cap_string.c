45d04827a8c608c9b504f16d9e0a137b853e3b50
NorthAlone <norbertlawadankossi@gmail.com>
2022-03-23 16:43:36 +0000

    update 3 script 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,8 +18,13 @@ char *cap_string(char *str)
 				str[i] = str[i] - 32;
 			continue;
 		}
-		if (str[i] == ' ')
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
 		{
+			if (str[i] == '\t')
+			{
+				str[i] = ' ';
+			}
+
 			++i;
 			if (str[i] >= 'a' && str[i] <= 'z')
 			{
