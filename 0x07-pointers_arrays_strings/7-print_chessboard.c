d81e33ee191e81e71c8624a53c81fcc9e6f565b9
NorthAlone <norbertlawadankossi@gmail.com>
2022-03-25 07:46:03 +0000

    add function print_chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: pointer to array
+ * Return: void print chessboard
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		if (i < 7)
+		{
+			printf("\n");
+		}
+	}
+}
