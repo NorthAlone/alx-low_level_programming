7faa97b9d00419c6eff3078895a4d77d81cdc1ea
NorthAlone <norbertlawadankossi@gmail.com>
2022-03-31 22:11:02 +0000

    add function 6-is_prime_number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * is_prime_number - check if number is a prime number
+ * @n: number to check
+ * Return: 1 if prime number 0 if not
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (search_prime_number(n, n - 1));
+}
+
+/**
+ * search_prime_number - check if number is a prime number
+ * @n: number to check
+ * @i: iterator
+ * Return: 1 if prime number 0 if not
+ */
+
+int search_prime_number(int n, i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (search_prime_number(n, i - 1));
+}
