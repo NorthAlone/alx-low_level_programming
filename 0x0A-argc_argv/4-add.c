942bc5a59392943a1ab7fa40c41e99ca37e3e349
NorthAlone <norbertlawadankossi@gmail.com>
2022-04-01 21:49:33 +0000

    update 4 program 4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,23 +4,20 @@
  * main - adds positive numbers
  * @argc: number of arguments
  * @argv: array of value of arguments
- * Return: always 0
+ *
+ * Return: 0 or 1 or sum
  */
 
 int main(int argc, char **argv)
 {
-	int result = 0, number, i, j, k;
+	int result = 0, number;
+	int i, j, k;
 
-	if (argc - 1 == 0)
-	{
-		printf("0\n");
-		return (0);
-	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] >= '9' || argv[i][j] < '0')
+			if (argv[i][j] > '9' || argv[i][j] < '0')
 			{
 				printf("Error\n");
 				return (1);
@@ -31,11 +28,8 @@ int main(int argc, char **argv)
 	for (k = 0; k < argc; k++)
 	{
 		number = atoi(argv[k]);
-
 		if (number >= 0)
-		{
 			result += number;
-		}
 	}
 	printf("%d\n", result);
 	return (0);
