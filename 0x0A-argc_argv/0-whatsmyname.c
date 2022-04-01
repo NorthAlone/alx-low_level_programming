6348b584ceb8b10caff589ae6fb456d8f6a5a24e
NorthAlone <norbertlawadankossi@gmail.com>
2022-04-01 12:18:23 +0000

    update 1 program 0-whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include "main.h"
+/**
+ * main - prints program name
+ * @argc: number of arguments
+ * @argv: array of value of arguments
+ * Return: always 0
+ */
 
 int main(int argc, char **argv)
 {
 	print("%s\n");
+	return (0);
 }
