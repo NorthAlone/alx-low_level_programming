cbde29e619366e5eb8d5d42bd547c8578e06550f
NorthAlone <norbertlawadankossi@gmail.com>
2022-04-03 23:31:50 +0000

    add function 104-fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the 50 fibonacci numerber
+ * Return: always 0 for sucess
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long int first = 1, second = 2, next = 0;
+
+	for (i = 1; i <= 98; i++)
+	{
+		if (i == 1)
+			printf("%lu, ", first);
+		if (i == 1)
+			printf("%lu, ", second);
+		if (i > 2)
+		{
+			next = first + second;
+			if (i < 98)
+				printf("%lu, ", next);
+			else
+				printf("%lu", next);
+
+			first = second;
+			second = next;
+			 next = 0;
+		}
+	}
+	printf("\n");
+	return (0);
+}
