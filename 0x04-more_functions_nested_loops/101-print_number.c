2268375c8b46fb4d71d830b2508c91739a23ae85
NorthAlone <norbertlawadankossi@gmail.com>
2022-04-04 02:32:13 +0000

    update 1 function 3-mul

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,28 +11,28 @@ void print_number(int n)
 	if (value < 0)
 	{
 		value = value * (-1);
-		putchar('-');
+		_putchar('-');
 	}
 	else if (value <= 9)
 	{
-		putchar(value + '0');
+		_putchar(value + '0');
 	}
 	else if (value > 9 && value <= 99)
 	{
-		putchar((value / 10) + '0');
-		putchar((value % 10) + '0');
+		_putchar((value / 10) + '0');
+		_putchar((value % 10) + '0');
 	}
 	else if (value > 99 && value <= 999)
 	{
-		putchar((value / 100) + '0');
-		putchar(((value / 10) % 10) + '0');
-		putchar((value % 10) + '0');
+		_putchar((value / 100) + '0');
+		_putchar(((value / 10) % 10) + '0');
+		_putchar((value % 10) + '0');
 	}
 	else if (value > 999)
 	{
-		putchar((value / 1000) + '0');
-		putchar(((value / 100) % 10) + '0');
-		putchar(((value / 10) % 10) + '0');
-		putchar((value % 10) + '0');
+		_putchar((value / 1000) + '0');
+		_putchar(((value / 100) % 10) + '0');
+		_putchar(((value / 10) % 10) + '0');
+		_putchar((value % 10) + '0');
 	}
 }
