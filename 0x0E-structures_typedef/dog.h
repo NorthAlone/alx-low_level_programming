22ba18f1afd893f0418e061c40f660c198f031d3
NorthAlone <norbertlawadankossi@gmail.com>
2022-04-11 23:48:22 +0000

    add dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog's information
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: dog's owner
+ *
+ * Description: all information about a dog
+ */
+
 struct dog
 {
 	char *name;
