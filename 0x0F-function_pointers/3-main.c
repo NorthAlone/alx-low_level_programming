a9d5e9d4af3f25f22535524dff432722fba2c70a
NorthAlone <norbertlawadankossi@gmail.com>
2022-04-14 01:33:55 +0000

    add A goal is not always meant to be reached task

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+/**
+ * errorHandle - handle error
+ * @value: exit value
+ */
+void errorHandle(int value)
+{
+	printf("Error\n");
+	exit(value);
+}
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: array of argument value
+ * Return: 0 for success
+ */
+
+int main(int argc, *argv[])
+{
+	int num1, num2, result;
+	char operator;
+	int (*calc)(int, int);
+
+	operator = argv[2];
+	if (argc != 4)
+		errorHandle(98);
+	calc = get_op_func(operator);
+
+	if (calc == NULL)
+		errorHandle(99);
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if ((operator == '%' || operator == '/') && num2 == 0)
+		errorHandle(100);
+
+	result = calc(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
